9b8151d2dd7a1c11aa6925a5488c3468d9092f1a
Pritschella <44498987+Pritschella@users.noreply.github.com>
2021-02-05 20:33:58 -0600

    Commit: "Se mejoró el algoritmo y se solucionó"

--- T1_A2_OperacionesBasicasConFunciones_Scala/src/AlgoritmoDeNewton.scala
+++ T1_A2_OperacionesBasicasConFunciones_Scala/src/AlgoritmoDeNewton.scala
@@ -4,24 +4,37 @@ object AlgoritmoDeNewton {
   
   var resultado = 0.0
   
-    def raizCuadrada(x:Double, y:Double): Unit = {
+  def cuociente(x:Double, y:Double): Double = {
+    x / y
+  }
+  
+  def promedio(cuociente:Double, y:Double): Double = {
+    (cuociente + y) / 2
+  }
+  
+  def dif(promedio:Double, x:Double): Boolean = {
+    var estimacion = promedio * promedio
+    var dif = estimacion - x
     
-      var cuociente = x/y
-      var promedio = (cuociente + y) /2
-      
-      var estimacion = promedio * promedio
-      var dif = x - estimacion
-        
-      if(dif<0.0001)
-        resultado = promedio
-      else
-        raizCuadrada(x, promedio)
-      }
+    if(dif<0.0001)
+      true
+    else
+      false
+  }
+  
+  def raizCuadrada(x:Double, y:Double): Unit = {
+    var prom = promedio(cuociente(x,y),y)
     
+    if(dif(prom, x))
+      resultado = prom
+    else
+      raizCuadrada(x,prom)
+  }
+  
     
-    def main(args: Array[String]): Unit = {
-      raizCuadrada(2,1)
-      println("El resultado es: " + resultado)
-    }
+  def main(args: Array[String]): Unit = {
+    raizCuadrada(2,1)
+    println("El resultado es: " + resultado)
+  }
   
 }
